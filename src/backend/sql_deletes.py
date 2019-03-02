7fbf136a169838da842a0d528e1c6be1a3a3eacb
Slavik Salo <slav27@gmail.com>
2019-03-01 17:32:13 -0800

    refactor current deletes to work with the new db design.

--- src/backend/sql_deletes.py
+++ src/backend/sql_deletes.py
@@ -1,17 +1,17 @@
 from sql_data_queries import * 
 def delete_item_type(cursor, type_name):
-    query = "DELETE FROM Item_Type WHERE type_name = %s"
-    cursor.execute(query, [type_name])
+    q = "DELETE FROM Item_Type WHERE type_name = %s"
+    cursor.execute(q, (type_name,))
 
 def delete_item(cursor , barcode):
-    query = "DELETE FROM Item WHERE barcode = %s"
-    cursor.execute(query, [barcode])
+    q = "DELETE FROM Item WHERE barcode = %s"
+    cursor.execute(q, (barcode,))
 
-def delete_author(cursor, first_name, last_name):
-    a_id = get_author_id(cursor, first_name, last_name)
-    query = "DELETE FROM Authors WHERE author_id = %s"
-    cursor.execute(query, (a_id,))
+def delete_author(cursor, first_name, last_name, middle_name=None):
+    a_id = get_author_id(cursor, first_name, last_name, middle_name)
+    q = "DELETE FROM Authors WHERE author_id = %s"
+    cursor.execute(q, (a_id,))
 
-def delete_bible(cursor, language, translation): 
-    q = ("DELETE FROM Bible WHERE language = %s AND translation = %s") 
-    cursor.execute(q, (language, translation))
+# def delete_bible(cursor, language, translation): 
+#     q = ("DELETE FROM Bible WHERE language = %s AND translation = %s") 
+#     cursor.execute(q, (language, translation))
