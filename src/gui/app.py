f220868f6c54f032d4729d487e5e44880cce19d6
Slavik Salo <slav27@gmail.com>
2019-04-24 23:57:13 -0700

    add logo, series checkbox is checkable by 'Enter' and 'SpaceBar' press, make changes to AddBookPage

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,7 +1,8 @@
 import mysql.connector
 from mysql.connector import errorcode
 from PyQt5.QtWidgets import QApplication, QStackedWidget, QStyleFactory
-from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore, QtWidgets
 from fubc_lib import Ui_StackedWidget
 
 import sys 
@@ -13,6 +14,7 @@ class MainWindow(QStackedWidget, Ui_StackedWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
         self.cnx = None 
         self.cursor = None
         self.loginBtn.clicked.connect(self.on_login)
@@ -21,7 +23,36 @@ class MainWindow(QStackedWidget, Ui_StackedWidget):
         self.login_error_label.hide()
         self.user_line.returnPressed.connect(self.on_enter_user_line)
         self.pass_line.returnPressed.connect(self.on_enter_pass_line)
-    
+        self.series_check_box.stateChanged.connect(self.on_in_series_checkbox)
+        self.setWindowIcon(QIcon('D:\\church_lib\\python_code\\img\\fubc_logo.jpg'))
+        self.series_check_box.installEventFilter(self)
+        
+        self.setFixedSize(self.size())
+
+    def eventFilter(self, o, e):
+        # if (o.objectName() == "series_check_box"):
+        if e.type() == QtCore.QEvent.KeyPress and e.key() ==  QtCore.Qt.Key_Return:
+            self.series_check_box.nextCheckState()
+            return True 
+        return False
+    # def eventFilter(self, o, e):
+    #     if e.type() == QEvent.DragEnter: #remember to accept the enter event
+    #         e.acceptProposedAction()
+    #         return True
+    #     if e.type() == QEvent.Drop:
+    #         # handle the event
+    #         # ...
+    #         return True
+    #     return False #remember to return false for other event types
+
+    def on_in_series_checkbox(self):
+        state = not self.series_label.isEnabled()
+        self.series_label.setEnabled(state)
+        self.in_series_label.setEnabled(state)
+        self.series_title_label.setEnabled(state)
+        self.series_title_line.setEnabled(state)
+        self.in_series_line.setEnabled(state)
+
     def on_enter_user_line(self):
         self.pass_line.setFocus()
 
@@ -90,6 +121,7 @@ class MainWindow(QStackedWidget, Ui_StackedWidget):
         # else:
         #     print('Closing Database Connection.')
         #     self.cnx.close()
+        
     def closeEvent(self, event):
         if self.cnx: 
             self.cnx.close()
