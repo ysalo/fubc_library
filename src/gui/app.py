e9c31ddad4cb0d48da3c61dacc0879f3393f27ff
Slavik Salo <slav27@gmail.com>
2019-04-24 18:32:12 -0700

    add error message to login page, connect addBookPage buttons

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,11 +1,13 @@
 import mysql.connector
 from mysql.connector import errorcode
 from PyQt5.QtWidgets import QApplication, QStackedWidget, QStyleFactory
+from PyQt5 import QtCore
 from fubc_lib import Ui_StackedWidget
 
 import sys 
 sys.path.insert(0, 'D:\\church_lib\\python_code\\src\\backend')
 import sql_data_queries as sdq
+import sql_inserts as si
  
 class MainWindow(QStackedWidget, Ui_StackedWidget):
     def __init__(self):
@@ -14,17 +16,60 @@ class MainWindow(QStackedWidget, Ui_StackedWidget):
         self.cnx = None 
         self.cursor = None
         self.loginBtn.clicked.connect(self.on_login)
+        self.add_book_btn.clicked.connect(self.on_add_book)
+        self.add_book_cancel_btn.clicked.connect(self.on_add_book_cancel)
+        self.login_error_label.hide()
+        self.user_line.returnPressed.connect(self.on_enter_user_line)
+        self.pass_line.returnPressed.connect(self.on_enter_pass_line)
     
+    def on_enter_user_line(self):
+        self.pass_line.setFocus()
+
+    def on_enter_pass_line(self):
+        self.loginBtn.click()
+
     def on_login(self):
         user_name = self.user_line.text()
         pass_word = self.pass_line.text()
         self.connect(user_name, pass_word)
         if self.cnx is None or not self.cnx.is_connected():
             print('Login error')
+            self.user_line.clear()
+            self.pass_line.clear()
+            self.user_line.setFocus()
+            self.login_error_label.show()
         else:
             print('Sucessful login') 
             self.setCurrentIndex(1)
 
+    def on_add_book(self):
+        title = self.book_title_line.text()
+        isbn = self.isbn_line.text()
+        language = self.language_box.currentText()
+        publication_year = self.pub_year_line.text()
+        genre_name = self.genre_box.currentText()
+        last_name = self.last_name_line.text()
+        first_name = self.first_name_line.text()
+        middle_name = self.middle_name_line.text()
+        series_title = self.series_title_line.text()
+        in_series_number = self.in_series_line.text()
+        barcode = self.barcode_line.text()
+
+        print("""Title: {} ISBN: {} Language: {} Publication Year: {} \nGenre: {} Last Name: {} First Name: {} Middle Name: {} 
+            Series Title: {} In Series Number: {} Barcode: {}""".format(title, isbn, language, publication_year, genre_name
+        ,last_name,first_name,middle_name,series_title,in_series_number,barcode))
+
+    def on_add_book_cancel(self):
+        self.book_title_line.clear()
+        self.isbn_line.clear()
+        self.pub_year_line.clear()
+        self.last_name_line.clear()
+        self.first_name_line.clear()
+        self.middle_name_line.clear()
+        self.series_title_line.clear()
+        self.in_series_line.clear()
+        self.barcode_line.clear()
+        self.book_title_line.setFocus()
 
     def connect(self, user_name, pass_word):
         try:
